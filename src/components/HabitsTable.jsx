9fe6232dd6149c5cbea2a64928e94c2601785a98
mathewsecure <mateosegurag845@gmail.com>
2025-08-18 23:46:30 -0600

    Update HabitsTable component to render the dates + add some code for testing

--- src/components/HabitsTable.jsx
+++ src/components/HabitsTable.jsx
@@ -6,10 +6,12 @@ const HabitsTable = () => {
   const [habits, setHabits] = useState(
     []
   ); /* Use an array to acces its functions, e.g., map. So we can render the inside later */
+  const [dates, setDates] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_HOST_TEST}/habits`, {
+      method: "GET",
       headers: {
         authorization: `Bearer ${import.meta.env.VITE_TOKEN_TEST}`,
       },
@@ -20,23 +22,37 @@ const HabitsTable = () => {
       });
   }, []);
 
-  const habitsPerPage = 10;
-  const indexOfLastHabit = currentPage * habitsPerPage;
-  const indexOfFirstHabit = indexOfLastHabit - habitsPerPage;
-  const currentHabits = habits.slice(indexOfFirstHabit, indexOfLastHabit);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_HOST_TEST}/dates`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${import.meta.env.VITE_TOKEN_TEST}`,
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setDates(data.dates); /* data is an object, {dates: Array(n)} */
+      });
+  }, []);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
-  const dateArray = [];
-  const date = new Date();
+  const habitsPerPage = 10;
+  const indexOfLastHabit = currentPage * habitsPerPage;
+  const indexOfFirstHabit = indexOfLastHabit - habitsPerPage;
+  const currentHabits = habits.slice(indexOfFirstHabit, indexOfLastHabit);
 
-  if (!dateArray.includes(date)) {
-    dateArray.push(date.toDateString());
+  const date = new Date();
+  var dateObjToString = dates.map((date) => date["date"]);
+
+  if (!dateObjToString.includes(date)) {
+    dateObjToString.push(date.toDateString());
   }
 
-  console.log(dateArray);
+  console.log("dates: ", dates);
+  console.log("dateObjToString: ", dateObjToString);
   console.log("currentPage: ", currentPage);
 
   return (
@@ -45,7 +61,7 @@ const HabitsTable = () => {
         <thead>
           <tr>
             <th>Habit</th>
-            <th>Completion date: {dateArray[currentPage - 1]}</th>
+            <th>Completion date: {dateObjToString[currentPage - 1]}</th>
           </tr>
         </thead>
         <tbody>
