58de1a56d8036bba25d3b0fc71734355c868617a
mathewsecure <mateosegurag845@gmail.com>
2026-01-12 22:59:18 -0600

    Add trend´s dummy bar chart

--- src/components/Trends/Trends.jsx
+++ src/components/Trends/Trends.jsx
@@ -1,5 +1,56 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const Trends = () => {
-  return <h2>Likely to be an habit</h2>;
+  const options = {
+    indexAxis: "y",
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const data = {
+    labels: ["habit", "habit2"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [65, 100],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgb(255, 99, 132)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h2>Likely to be an habit</h2>
+      <Bar data={data} options={options} />
+    </div>
+  );
 };
 
 export default Trends;
